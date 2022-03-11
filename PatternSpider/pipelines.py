ea1359213a3e564936e2369690b8b6815e987e13
bantu_LFF <lff18731218157@163.com>
2022-03-11 09:43:50 +0800

    测试远程机器同步问题

--- PatternSpider/pipelines.py
+++ PatternSpider/pipelines.py
@@ -9,11 +9,6 @@
 
 # Copyright (C) 2022 北京盘拓数据科技有限公司 All Rights Reserved
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import json
 import os
 import traceback
