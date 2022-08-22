5283cd94759c04bc20c8ca9697655216c6d5e100
bantu_LFF <liufenfan@patterntech.cn>
2022-08-22 15:34:43 +0800

    chrome 加代理

--- PatternSpider/selenium_manage/base_chrome.py
+++ PatternSpider/selenium_manage/base_chrome.py
@@ -11,6 +11,7 @@
 import json
 import os
 import random
+import re
 import time
 
 import requests
@@ -25,11 +26,56 @@ from PatternSpider.selenium_manage import BaseSelenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as Wait
+import zipfile
 
 settings = get_project_settings()
 
 
 class BaseChrome(BaseSelenium):
+    CHROME_PROXY_HELPER_DIR = 'Chrome-proxy-helper'
+    # 存储自定义Chrome代理扩展文件的目录
+    CUSTOM_CHROME_PROXY_EXTENSIONS_DIR = 'chrome-proxy-extensions'
+
+    def get_chrome_proxy_extension(self, proxy):
+        """
+        获取一个Chrome代理扩展,里面配置有指定的代理(带用户名密码认证)
+        proxy - 指定的代理,格式: username:password@ip:port
+        """
+        m = re.compile('([^:]+):([^\@]+)\@(.+):(\d+)').search(proxy)
+        if m:
+            # 提取代理的各项参数
+            username = m.groups()[0]
+            print(username)
+            password = m.groups()[1]
+            print(password)
+            ip = m.groups()[2]
+            print(ip)
+            port = m.groups()[3]
+            print(port)
+
+            # 创建一个定制Chrome代理扩展(zip文件)
+            if not os.path.exists(self.CUSTOM_CHROME_PROXY_EXTENSIONS_DIR):
+                os.mkdir(self.CUSTOM_CHROME_PROXY_EXTENSIONS_DIR)
+            extension_file_path = os.path.join(self.CUSTOM_CHROME_PROXY_EXTENSIONS_DIR,
+                                               '{}.zip'.format(proxy.replace(':', '_')))
+
+            if not os.path.exists(extension_file_path):
+                # 扩展文件不存在，创建
+                zf = zipfile.ZipFile(extension_file_path, mode='w')
+                zf.write(os.path.join(self.CHROME_PROXY_HELPER_DIR, 'manifest.json'), 'manifest.json')
+
+                # 替换模板中的代理参数
+                background_content = open(os.path.join(self.CHROME_PROXY_HELPER_DIR, 'background.js')).read()
+                background_content = background_content.replace('%proxy_host', ip)
+                background_content = background_content.replace('%proxy_port', port)
+                background_content = background_content.replace('%username', username)
+                background_content = background_content.replace('%password', password)
+                zf.writestr('background.js', background_content)
+                zf.close()
+
+            return extension_file_path
+        else:
+            raise Exception('Invalid proxy format. Should be username:password@ip:port')
 
     def clear_cache_driver(self):
         """
@@ -67,8 +113,7 @@ class BaseChrome(BaseSelenium):
         options._debugger_address = "localhost:9992"
         return webdriver.Chrome(chrome_options=options)
 
-    @staticmethod
-    def get_log_options(headless):
+    def get_log_options(self, headless):
         option = webdriver.ChromeOptions()
         option.add_argument('--no-sandbox')
         option.add_argument('--disable-gpu')
@@ -91,6 +136,9 @@ class BaseChrome(BaseSelenium):
             'enableNetwork': True,
             'enablePage': False,
         })
+        option.add_extension(self.get_chrome_proxy_extension(
+            proxy="liufeifantest-zone-isp-session-3129z7{}-sessTime-180:961206@proxy.ipidea.io:2336".format(
+                random.randint(100, 1000))))
         return option
 
     @staticmethod
